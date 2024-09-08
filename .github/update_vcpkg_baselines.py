5e6f1b2eea35e3eb03bf4216ace769e78ad41b35
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2024-09-07 23:59:13 -0700

    add path conditions to repository dispatch workflow

--- .github/update_vcpkg_baselines.py
+++ .github/update_vcpkg_baselines.py
@@ -12,7 +12,7 @@ vcpkg_ref = (
     .split()[0]
 )
 
-clib_vcpkg_registry_ref = (
+custom_vcpkg_registry_ref = (
     run(["git", "ls-remote", custom_vcpkg_registry_repo], capture_output=True)
     .stdout.decode()
     .splitlines()[0]
@@ -23,7 +23,9 @@ with open("./vcpkg.json", "r") as f:
     vcpkg_json = load(f)
 
 vcpkg_json["builtin-baseline"] = vcpkg_ref
-vcpkg_json["vcpkg-configuration"]["registries"][0]["baseline"] = clib_vcpkg_registry_ref
+vcpkg_json["vcpkg-configuration"]["registries"][0]["baseline"] = (
+    custom_vcpkg_registry_ref
+)
 
 with open("./vcpkg.json", "w") as f:
     dump(vcpkg_json, f, indent=2)
